59e7df05fa387a79ba3c8b0dc5e1999be0d0dcee
Vasilii Nikonov <nikonov.va@yandex.ru>
2025-10-08 13:39:22 -0700

    improved the readability for the logic of working with different flags in main

--- src/main.rs
+++ src/main.rs
@@ -28,22 +28,12 @@ enum Expr {
     BinOp(Op2, Box<Expr>, Box<Expr>)
 }
 
-// TODO: ask about this Atom/List thing, as 
 fn parse_expr(s: &Sexp) -> Expr {
     match s {
         Sexp::Atom(I(n))                                                       => Expr::Number(i32::try_from(*n).unwrap()),
         Sexp::Atom(S(s))                                                       => Expr::Id(s.clone()),
         Sexp::List(vec) => {
             match &vec[..] {
-
-        // TODO: this didn't work out, why?, said I cannot use a macro inside a pattern
-        /*                 match b {
-                            Sexp::List(vec![Sexp::Atom(S(name)), e])           => {
-                                let pair = (name.clone(), parse_expr(e));
-                                bs.push(pair);
-                            }
-                            _                                                  => panic!("Invalid") 
-        */
                 [Sexp::Atom(S(op)), Sexp::List(bindings), body] if op == "let" => {
                     let mut bs = Vec::new();
                     for b in bindings {
@@ -81,15 +71,10 @@ fn compile_expr(e: &Expr, si: i32, env: HashMap<String, i32>) -> String {
         Expr::Id(s)             => format!("mov rax, [rsp - {}]", env[s] * 8),
         Expr::Let(bs, body)     => {
             let mut result_instr = String::new();
-            // TODO: do I actually need the mutable copies right here?
             let mut curr_si = si;
             let mut curr_env = env.clone();
 
             for (v, e) in bs {
-                /* We do not have an acess to the variable when we are compiling the expression inside the 
-                 * binding, so we shouldn't be updating it yet;
-                 * It would only be acessible inside the body
-                 */ 
                 let e_instr = compile_expr(e, si, env.clone());
                 let store_curr_value_instr = format!("mov [rsp - {}], rax", curr_si * 8);
 
@@ -134,12 +119,12 @@ fn compile_expr(e: &Expr, si: i32, env: HashMap<String, i32>) -> String {
 
 fn compile_ops(e : &Expr, ops : &mut dynasmrt::x64::Assembler, si : i32, env: HashMap<String, i32>) {
     match e {
-        Expr::Number(n) => { dynasm!(ops ; .arch x64 ; mov rax, *n); }
-        Expr::Id(s)     => {
+        Expr::Number(n)        => { dynasm!(ops ; .arch x64 ; mov rax, *n); }
+        Expr::Id(s)            => {
             let stack_offset = env[s] * 8;
             dynasm!(ops ; .arch x64 ; mov rax, [rsp - stack_offset]);
         }
-        Expr::Let(bs, body) => {
+        Expr::Let(bs, body)    => {
             let mut curr_si = si;
             let mut curr_env = env.clone();
 
@@ -152,13 +137,13 @@ fn compile_ops(e : &Expr, ops : &mut dynasmrt::x64::Assembler, si : i32, env: Ha
             }
             compile_ops(&body, ops, curr_si, curr_env.clone());
         }
-        Expr::UnOp(t, e) => {
+        Expr::UnOp(t, e)       => {
             match t {
-                Op1::Add1 => { 
+                Op1::Add1      => { 
                     compile_ops(&e, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; add rax, 1); 
                 }
-                Op1::Sub1 => {
+                Op1::Sub1      => {
                     compile_ops(&e, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; sub rax, 1); 
                 }
@@ -166,21 +151,21 @@ fn compile_ops(e : &Expr, ops : &mut dynasmrt::x64::Assembler, si : i32, env: Ha
         } 
         Expr::BinOp(t, e1, e2) => {
             match t {
-                Op2::Plus => {
+                Op2::Plus      => {
                     let stack_offset = si * 8;
                     compile_ops(&e1, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; mov [rsp - stack_offset], rax);
                     compile_ops(&e2, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; add rax, [rsp - stack_offset]);
                 }
-                Op2::Minus => {
+                Op2::Minus     => {
                     let stack_offset = si * 8;
                     compile_ops(&e2, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; mov [rsp - stack_offset], rax);
                     compile_ops(&e1, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; sub rax, [rsp - stack_offset]);
                 }
-                Op2::Times => {
+                Op2::Times     => {
                     let stack_offset = si * 8;
                     compile_ops(&e1, ops, si, env.clone());
                     dynasm!(ops ; .arch x64 ; mov [rsp - stack_offset], rax);
@@ -192,53 +177,86 @@ fn compile_ops(e : &Expr, ops : &mut dynasmrt::x64::Assembler, si : i32, env: Ha
     }
 }
 
-
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        eprintln!("Usage: cargo run [CARGO_FLAGS] -- (-c |-e |-g) <input.snek> <output.s>");
-    }
-
-    let flag = &args[1];
-    let in_name = &args[2];
-
+fn file_to_expr(in_name: &str) -> std::io::Result<Expr> {
     let mut in_file = File::open(in_name)?;
     let mut in_contents = String::new();
     in_file.read_to_string(&mut in_contents)?;
-    let expr = parse_expr(&parse(&in_contents).unwrap());
+    Ok(parse_expr(&parse(&in_contents).unwrap()))
+}
 
-    match flag.as_str() {
-        "-c" => {
-            let out_name = &args[3];
-            if args.len() < 4 {
-                eprintln!("Usage: cargo run [CARGO_FLAGS] -- -c <input.snek> <output.s>");
-            }
-            let env = HashMap::new();
-            let result = compile_expr(&expr, 2, env.clone());
-            let asm_program = format!("
+fn generate_mode(in_name: &str, out_name: &str) -> std::io::Result<()> {
+    let expr = file_to_expr(in_name)?;
+
+    let env = HashMap::new();
+    let result = compile_expr(&expr, 2, env.clone());
+    let asm_program = format!("
 section .text
 global our_code_starts_here
 our_code_starts_here:
   {}
   ret
 ", result);
-            let mut out_file = File::create(out_name)?;
-            out_file.write_all(asm_program.as_bytes())?;
+    let mut out_file = File::create(out_name)?;
+    out_file.write_all(asm_program.as_bytes())?;
 
+    Ok(())
+}
+
+fn eval_mode(in_name: &str) -> std::io::Result<()> {
+    let expr = file_to_expr(in_name)?;
+
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+    let start = ops.offset();
+
+    let env = HashMap::new();
+    compile_ops(&expr, &mut ops, 2, env.clone());
+
+    dynasm!(ops ; .arch x64 ; ret);
+    let buf = ops.finalize().unwrap();
+    let jitted_fn: extern "C" fn() -> i64 = unsafe { mem::transmute(buf.ptr(start)) };
+    let result = jitted_fn();
+    println!("{}", result);
+    Ok(())
+}
+
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: cargo run [CARGO_FLAGS] -- -c <input.snek> <output.s>");
+        eprintln!("Usage: cargo run [CARGO_FLAGS] -- -e <input.snek>");
+        eprintln!("Usage: cargo run [CARGO_FLAGS] -- -g <input.snek> <output.s>");
+    }
+
+    let flag = &args[1];
+
+    match flag.as_str() {
+        "-c" => {
+            if args.len() < 4 {
+                eprintln!("Usage: cargo run [CARGO_FLAGS] -- -c <input.snek> <output.s>");
+            }
+
+            let in_name = &args[2];
+            let out_name = &args[3];
+
+            let _ = generate_mode(in_name, out_name);
         },
         "-e" => {
-            let mut ops = dynasmrt::x64::Assembler::new().unwrap();
-            let start = ops.offset();
+            if args.len() < 3 {
+                eprintln!("Usage: cargo run [CARGO_FLAGS] -- -e <input.snek>");
+            }
+            let in_name = &args[2];
+            let _ = eval_mode(in_name);
 
-            let env = HashMap::new();
-            compile_ops(&expr, &mut ops, 2, env.clone());
+        },
+        "-g" => {
+            if args.len() < 4 {
+                eprintln!("Usage: cargo run [CARGO_FLAGS] -- -g <input.snek>");
+            }
 
-            dynasm!(ops ; .arch x64 ; ret);
-            let buf = ops.finalize().unwrap();
-            let jitted_fn: extern "C" fn() -> i64 = unsafe { mem::transmute(buf.ptr(start)) };
-            let result = jitted_fn();
-            println!("{}", result);
+            let in_name = &args[2];
+            let out_name = &args[3];
         },
         _    => {
             eprintln!("Unknown flag: {}", flag);
